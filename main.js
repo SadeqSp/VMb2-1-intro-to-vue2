81fc30492babb57d188568c8539007c3c38ca4d1
SadeqSp <sadeq.sp@gmail.com>
2021-05-31 21:29:46 +0430

    7-vue-computed-properties

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',   
         product: 'Socks',
-        image: './assets/g.jpg',
-        inStock: true,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-natural"],
         variants: [
-            {variantId: 2234, variantColor: "green", variantImage: './assets/g.jpg'},
-            {variantId: 2235, variantColor: "blue",  variantImage: './assets/b.jpg'}
+            {variantId: 2234, variantColor: "green", variantImage: './assets/g.jpg', variantQuantity: 10},
+            {variantId: 2235, variantColor: "blue",  variantImage: './assets/b.jpg', variantQuantity: 0}
         ],
         cart: 0
     },
-    methods: {
-        addToCart: function() {
+    methods: {         
+        addToCart: function() {             
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {       
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        }, 
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
