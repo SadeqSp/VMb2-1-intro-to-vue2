21e72512a2bcad150e7e7ac915913d260fa36a2a
SadeqSp <sadeq.sp@gmail.com>
2021-05-31 21:19:52 +0430

    5-event-handling

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ var app = new Vue({
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-natural"],
         variants: [
-            {variantId: 2234, variantColor: "green"},
-            {variantId: 2235, variantColor: "blue"}
-        ]
+            {variantId: 2234, variantColor: "green", variantImage: './assets/g.jpg'},
+            {variantId: 2235, variantColor: "blue",  variantImage: './assets/b.jpg'}
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
