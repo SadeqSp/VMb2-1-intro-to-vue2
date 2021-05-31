b5fea5a75f76da416799f34732241756c7c8ed64
SadeqSp <sadeq.sp@gmail.com>
2021-05-31 21:53:19 +0430

    9-communicating-events

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ Vue.component('product', {
             </div>
 
             <div class="product-info">
-                <h1>{{ title }}</h1>
+                <h1>{{ title }}</h1> 
                 <p v-if="inStock">In Stock</p>
                 <p v-else>Out of Stock</p>
                 <p>User is premium: {{ premium }}</p>
@@ -35,44 +35,57 @@ Vue.component('product', {
 
                 <product-details :details="details"></product-details>
 
-                <div v-for="(variant, index) in variants"
+                <div v-for="(variant, index) in variants" 
                     :key="variant.variantId"
                     class="color-box"
-                    :style="{backgroundColor: variant.variantColor}"
+                    :style="{backgroundColor: variant.variantColor}"       
                     @mouseover="updateProduct(index)">
                 </div>
 
-                <button v-on:click="addToCart"
-                        :disabled="!inStock"
-                        :class="{ disabledButton: !inStock }"
+                <button v-on:click="addToCart" 
+                        :disabled="!inStock"             
+                        :class="{ disabledButton: !inStock }"   
                         >add to cart</button>
-                <div class="cart">
-                    <p>Cart({{ cart }})</p>
-                </div>
-
+                <button @click="decCart">delete</button>
             </div>
-
         </div>
     `,
-    data() {        // 'data' is now a function that returns data
+    data() {        
         return {
-            brand: 'Vue Mastery',
+            brand: 'Vue Mastery',    
             product: 'Socks',
-            selectedVariant: 0,      
-            details: ["80% cotton", "20% polyester", "Gender-natural"],     // this data will be send like 'premium' (from app) as value in <product-details>
+            selectedVariant: 0,         
+            details: ["80% cotton", "20% polyester", "Gender-natural"], 
             variants: [
-                {variantId: 2234, variantColor: "green", variantImage: './assets/g.jpg', variantQuantity: 10},
-                {variantId: 2235, variantColor: "blue",  variantImage: './assets/b.jpg',  variantQuantity: 0}
-            ],
-            cart: 0
+                {variantId: 2234, variantColor: "green", variantImage: './assets/g.jpg', variantQuantity: 10}, 
+                {variantId: 2235, variantColor: "blue",  variantImage: './assets/b.jpg',  variantQuantity: 5}
+            ]
         }
     },
     methods: {      // just like Vue/app         
         addToCart: function() {             
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)   
+                                        // ** the second param will be added (and explained) later in lesson
+                                        // at first we dont have it
+
+                                        // 1  -  emitting an event ('add-to-cart' is the name of the new event > using in html)
+                                        // we should let the parent [the app] know if the button is clicked (because the cart is no longer in compoenent but in 'app')
+                                        // by click on btn > do 'addToCart' [in component's template]
+                                        // the 'addToCart' is emitted to 'add-to-cart' event
+                                        // in [app] > when the 'add-to-cart'/event happens > do the 'updateCart' [a method from the app]
+                                        // so if we have more than one instance of our 'product' component all click changes our only CART
+
+                                        // 2  - by adding variantId 9as the 2nd param) we get the id of added product and send it to 'updateCart'/method  
+                                        // by now the id of added product will be added to 'cart'/array
+                                        // but this isn't good 
+                                        //  so we show the length of our array in HTML not the whole array >> {{ cart.length }}
+                                        // then we can see the added product's ids in chrome's Vue.devTool
         },
         updateProduct(index) {       
             this.selectedVariant = index
+        },
+        decCart() {
+            this.$emit('del-from-cart', this.variants[this.selectedVariant].variantId)
         }
     },
     computed: {     // just like Vue/app
@@ -98,8 +111,22 @@ Vue.component('product', {
 var app = new Vue({         
     el: '#app',
     data: {
-        premium: true   // [HTML > :premium(prop)="premium(this data)"]
-                        // < this data will be send as a value for 'premium'/attr, to the component's prop  
-                        // then be used in template  >>  <p>{{ premium }}</p> 
+        premium: true,          
+        //cart: 0       // this the old version (beginning of lesson)
+                        // for knowing wich item is added to cart we use an array instead of '0' (next line)
+        cart: []        // then in the methods (cont.: below)
+    }, 
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)      // we push the id of added product 
+                                    // now we should send 'id' to this method from our component when emitting the 'add-to-cart' event
+        }, 
+        deleteFrom(id) {
+            for(var i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                   this.cart.splice(i, 1);
+                }
+            }   
+        }
     }
 })
